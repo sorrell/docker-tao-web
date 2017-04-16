02b2e9b8f318f754f5f771fe946579256d210475
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2017-04-16 10:54:16 +0000

    docker-compose v2, db creation from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 export TAO_SERVER_NAME=188.226.145.196
-export TAO_POSTGRES_USER=postgres
 export TAO_POSTGRES_DB=taodb
+export TAO_POSTGRES_USER=postgres
 export TAO_POSTGRES_PASSWORD=dbpass
 
+echo
+echo -- Build TAO image
 #docker build --build-arg TAO_SERVER_NAME=$TAO_SERVER_NAME -t sorrell/docker-tao-web .
-#docker-compose up -d
 
+echo
+echo -- Up TAO and Postgres images
+docker-compose up -d
+
+echo
+echo -- Wait 5 second Postgres to up and run
+sleep 5
+
+echo
+echo -- Create TAO database
 ID=$(docker ps | grep "postgres" | awk '{print $1}')
-docker exec $ID createdb -U $TAO_POSTGRES_USER $TAO_POSTGRES_DB
+docker exec -it $ID createdb -U ${TAO_POSTGRES_USER} ${TAO_POSTGRES_DB}
