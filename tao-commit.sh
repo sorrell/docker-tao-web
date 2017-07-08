f297134459dd5a9603e04012fb98dffc550781ae
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2017-07-08 19:42:59 +0300

    Update tao-commit.sh

--- tao-commit.sh
+++ tao-commit.sh
@@ -2,5 +2,5 @@
 
 TAO_VERSION=latest
 
-docker commit dockertaoweb_tao_1 tao-web:$TAO_VERSION
-docker commit dockertaoweb_pgdb_1 tao-db:$TAO_VERSION
+sudo docker commit dockertaoweb_tao_1 tao-web:$TAO_VERSION
+sudo docker commit dockertaoweb_pgdb_1 tao-db:$TAO_VERSION
