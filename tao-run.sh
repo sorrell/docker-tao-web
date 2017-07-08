0be63c142b1c057126f394fb34515279ad0548ef
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2017-07-08 19:43:23 +0300

    Update tao-run.sh

--- tao-run.sh
+++ tao-run.sh
@@ -2,5 +2,5 @@
 
 TAO_VERSION=latest
 
-docker run -d --name tao-db tao-db:$TAO_VERSION
-docker run -d -p 80:80 --name tao-web --link tao-db:pgdb tao-web:$TAO_VERSION
+sudo docker run -d --name tao-db tao-db:$TAO_VERSION
+sudo docker run -d -p 80:80 --name tao-web --link tao-db:pgdb tao-web:$TAO_VERSION
